006ec745da96b26a821d3a2992d3a674476f5d3b
aashayk18 <aashayk100@gmail.com>
2023-10-17 16:48:01 +0530

    problem solving 101

---
+++ BetTwoSets.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    end = min(b) +1
+    start = max(a)
+    result = 0
+        
+    for i in range(start, end):
+        if all(not i % a_element for a_element in a) and all(not b_element % i for b_element in b):
+            result += 1
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
